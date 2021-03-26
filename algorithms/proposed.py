abc1d909ca2d746334dba8524f175c46de3af34e
deeplearning2020 <yeahia.ruet@gmail.com>
2021-03-27 02:29:00 +0600

    fdf

--- algorithms/proposed.py
+++ algorithms/proposed.py
@@ -100,7 +100,7 @@ def get_model_compiled(shapeinput, num_class, w_decay=0):
         3, 3), padding='same', strides=1)(x)
     #x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    x = Dropout(0.4)(x)
+    x = Dropout(0.7)(x)
     #x = Flatten()(x)
     x = GlobalAveragePooling2D()(x)
     x = Dense(units=128,kernel_regularizer=regularizers.l2(w_decay))(x)
