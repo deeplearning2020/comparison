8a713bb2e0f05bc08131aa9747ada76272cab7f1
deeplearning2020 <yeahia.ruet@gmail.com>
2021-05-23 15:20:46 +0600

    red

--- algorithms/proposed.py
+++ algorithms/proposed.py
@@ -81,7 +81,7 @@ def set_params(args):
 
 
 def get_model_compiled(shapeinput, num_class, w_decay=0):
-    inputs = Input((shapeinput[0],shapeinput[1],shapeinput[2]))
+    inputs = Input((shapeinput[0],shapeinput[1],shapeinput[2]),1)
     filters = [4,4,4,8]
     x = Conv3D(filters=4,use_bias=False,kernel_size=(3,3,5), padding =       'valid',strides = 1)(inputs)
     x = BatchNormalization()(x)
